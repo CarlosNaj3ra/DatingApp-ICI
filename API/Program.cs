c200bb7d7d87d58004375c61c9c7dac4f92c1e86
CarlosNaj3ra <cnajerar2906@gmail.com>
2024-09-24 19:41:01 -0600

    Parcial01-AddEFMigrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,12 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
-
-app.MapControllers();
-
-app.Run();
