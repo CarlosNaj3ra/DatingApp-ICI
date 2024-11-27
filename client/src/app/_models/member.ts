948af4bb2fb42f273494de532293ed4935551310
practicantearq03 <techie12@innovatechminds.com>
2024-11-26 20:17:19 -0600

    Parcial03 - Add routing to member detail page

--- client/src/app/_models/member.ts
+++ client/src/app/_models/member.ts
@@ -1,7 +1,8 @@
 import { Photo } from "./photo";
+
 export interface Member {
     id: number;
-    username: string;
+    userName: string;
     age: number;
     photoUrl: string;
     knownAs: string;
