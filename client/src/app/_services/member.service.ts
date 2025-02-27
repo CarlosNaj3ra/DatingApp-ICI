11c8935f0c83ff0f908e318eed6c2fe5b23be85c
practicantearq03 <101608690+CarlosNaj3ra@users.noreply.github.com>
2025-02-26 23:30:35 -0600

    Parcial04 - Set main photo in client

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -49,16 +49,7 @@ export class MembersService {
     );
   }
 
-  deletePhoto(photo: Photo) {
-    return this.http.delete(this.baseUrl + "users/photo/" + photo.id).pipe(
-      tap(() => {
-        this.members.update(members => members.map(m => {
-          if (m.photos.includes(photo)) {
-            m.photos = m.photos.filter(p => p.id !== photo.id)
-          }
-          return m;
-        }))
-      })
-    );
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + "users/photo" + "photoId");
   }
 }
