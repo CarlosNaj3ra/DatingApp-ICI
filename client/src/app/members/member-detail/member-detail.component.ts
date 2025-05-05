80ebb953884f8aaedf5fec4340493581c36b44a2
practicantearq03 <101608690+CarlosNaj3ra@users.noreply.github.com>
2025-05-05 13:41:48 -0600

    Parcial05 - Add message thread to SPA

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MembersService } from '../../_services/member.service';
+import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TimeagoModule } from 'ngx-timeago';
 import { DatePipe } from '@angular/common';
+import { MemberMessagesComponent } from "../member-messages/member-messages.component";
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe],
+  imports: [TabsModule, GalleryModule, TimeagoModule, DatePipe, MemberMessagesComponent],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -26,6 +27,8 @@ export class MemberDetailComponent implements OnInit{
 
   loadMember() {
     const username = this.route.snapshot.paramMap.get("username");
+    console.log("Username:" + username);
+
     if (!username) return;
     this.memberService.getMember(username).subscribe({
       next: (member) => {
