e95e2a736c1a4db11a71282568b2d1c5c30aa586
CarlosNaj3ra <101608690+CarlosNaj3ra@users.noreply.github.com>
2025-06-17 15:03:42 -0600

    Parcial06 - Update start up class

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,17 +1,24 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-
 namespace API.Extensions;
+using System.Text;
+using API.Data;
+using API.DataEntities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 public static class IdentityServiceExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddIdentityCore<AppUser>(opt => opt.Password.RequireNonAlphanumeric = false)
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
-                var tokenKey = config["TokenKey"] ?? throw new ArgumentNullException("TokenKey");
+                var tokenKey = config["TokenKey"] ?? throw new ArgumentException("TokenKey");
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
                     ValidateIssuerSigningKey = true,
